20517ef236f3c53bc7bae60f976f298e793a97e2
BeratCeylan07 <berat.ceylan@kod.com.tr>
2026-06-23 21:58:48 +0300

    Fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bbjFoqqv7XqwtB1mjMH5tbXyHgRAamXobuD0q1V6eNU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
